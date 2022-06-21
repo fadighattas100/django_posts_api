270ad7a30a270f4a693a099af0a92dbfcc970bf8
Fadi Ghattas <ghattas@compredict.de>
2022-06-21 12:11:41 +0200

    updates

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
-import setuptools
+import os
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+this_dir = os.path.abspath(os.getcwd())
 
-setuptools.setup(
+with open(os.path.join(this_dir, "README.md"), "rb") as fo:
+    long_description = fo.read().decode("utf8")
+
+setup(
     name="django_posts_api",
-    version="0.0.2",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    version="0.0.3",
+    author="Fadi Ghattas",
+    author_email="fadighattas100@outlook.com",
+    description="A small example plugin package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/fadighattas100/django_posts_api",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/fadighattas100/django_posts_api/issues",
     },
+    download_url="https://github.com/fadighattas100/django_posts_api/blob/master/dist/django_posts_api-0.0.3.tar.gz",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    packages=find_packages(where="src"),
     python_requires=">=3.8",
-    # install_requires=[
-    #     'Django==4.0.4',
-    # ]
+    include_package_data=True,
+    install_requires=[],
+    extras_require={
+        'dev': []
+    }
 )
